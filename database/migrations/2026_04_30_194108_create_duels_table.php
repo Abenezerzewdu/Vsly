0b7a2477cc3b8075afd457db3e451af4d0c768c1
abenezer <abenezerzewdu11@gmail.com>
2026-05-04 01:35:35 +0300

    added total rounds column

--- database/migrations/2026_04_30_194108_create_duels_table.php
+++ database/migrations/2026_04_30_194108_create_duels_table.php
@@ -36,7 +36,7 @@ return new class extends Migration
         ->nullOnDelete();
 
     $table->unsignedTinyInteger('current_round')->default(1);
-
+    $table->unsignedTinyInteger('total_rounds')->default(3);
     $table->timestamps();
 });
     }
